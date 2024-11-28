8795798bf32518c7433fea06faad3f86aa1e7dbf
Taylor Mravic <taylor.mravic@gmail.com>
2024-11-28 10:17:46 +0900

    Rails 8 Authentication

---
+++ app/models/user.rb
@@ -0,0 +1,14 @@
+class User < ApplicationRecord
+  has_secure_password
+  has_many :sessions, dependent: :destroy
+
+  normalizes :email_address, with: ->(e) { e.strip.downcase }
+
+  enum :role, [:user, :moderator, :admin]
+
+  after_initialize :set_default_role, :if => :new_record?
+
+  def set_default_role
+    self.role ||= :user
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resource :session
+  resources :passwords, param: :token
   root "pages#home"
   get "pages/home"
 
