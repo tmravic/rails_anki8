ae9b5b8ad1dacaf509298c5003b259907100ec5d
Taylor Mravic <taylor.mravic@gmail.com>
2024-12-07 10:01:27 +0900

    Add before_save callback on User

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,7 @@ class User < ApplicationRecord
   enum :role, [:user, :moderator, :admin]
 
   after_initialize :set_default_role, :if => :new_record?
+  before_save -> { puts "Before saving #{self}" }
 
   def set_default_role
     self.role ||= :user
