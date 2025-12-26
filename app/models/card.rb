d4ef3d212b5572d032decfa6cf32e58de4f599af
Taylor Mravic <taylor.mravic@gmail.com>
2025-12-26 10:41:24 +0900

    Add schema info to Card model

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,3 +1,11 @@
+# create_table "cards", force: :cascade do |t|
+#   t.bigint "user_id", null: false
+#   t.string "card_number"
+#   t.datetime "created_at", null: false
+#   t.datetime "updated_at", null: false
+#   t.index ["user_id"], name: "index_cards_on_user_id"
+# end
+
 class Card < ApplicationRecord
   belongs_to :user, optional: true
 end
