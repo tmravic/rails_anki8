f88ac08496c484d22c2bedb7272b75948fa16582
Taylor Mravic <taylor.mravic@gmail.com>
2026-05-30 15:30:21 +0900

    Call has_mobile_fu(true) + add detection debug
    Calls has_mobile_fu(true) and adds basic mobile detection debug.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,17 @@
 class ApplicationController < ActionController::Base
   include Authentication
+
+  # Enable the mobile-fu gem.
+  #
+  # What this does:
+  # - Includes ActionController::MobileFu into this controller
+  # - Registers several helper methods (is_mobile_device?, mobile_device, etc.)
+  # - Adds a before_action called :set_request_format (by default)
+  #
+  # Passing `true` means "yes, automatically try to set the request format".
+  # We will later override set_mobile_format to get more control.
+  has_mobile_fu(true)
+
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
 
